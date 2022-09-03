037633ff2c1f8648dba91c8d47f65bf62acd4639
Gazda70 <pjgazda@interia.pl>
2022-09-03 19:40:40 +0200

    Add basic note counting.

--- midi_experiments/midi_experiments.py
+++ midi_experiments/midi_experiments.py
@@ -1,22 +1,41 @@
 from mido import MidiFile
 import paths as pth
-
+from signature_of_fifths_functions import SignatureOfFifthsUtility
 
 mid = MidiFile(pth.MIDI_FILES + 'VampireKillerCV1.mid', clip=True)
 print(mid)
+print("MIDI type: " + str(mid.type))
 
+'''
 for track in mid.tracks:
     print(track)
 
 for msg in mid.tracks[0]:
     print(msg)
-
+'''
+sig_util = SignatureOfFifthsUtility()
+print("NUMBER OF TRACKS: " + str(len(mid.tracks)))
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    '''
+    for msg in track:
+        print(msg)
+    '''
 for track in mid.tracks:
+    print("\n")
     print("TRACK\n")
     print(track)
     print("\n")
+    print("NOTES\n")
+    notes_dict = sig_util.count_notes_in_track(track)
+    for x, y in notes_dict.items():
+        print(x, y)
+    print("\n")
+    '''
     print("MESSAGES")
     print("\n")
     for msg in track:
+        print(msg.type)
         print(msg)
     print("\n\n")
+    '''
