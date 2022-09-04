b8f6f4a8eb6e00994ec8c4b5663fa8aa3c83db6b
Gazda70 <pjgazda@interia.pl>
2022-09-04 20:16:37 +0200

    Add signature of fifths calculation and basic visualisation.

---
+++ functions/signature_of_fifths_functions.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from collections import Counter
+from dataclasses import dataclass
+from mido import MidiFile
+
+"""
+    It has to be in such order because of the numeration of notes used by MIDI standard.
+"""
+
+
+class Note(Enum):
+    C, C_SHARP, D, D_SHARP, E, F, F_SHARP, G, G_SHARP, A, A_SHARP, B = range(12)
+
+
+class NoteVectorDirection(Enum):
+    A_DIR, D_DIR, G_DIR, C_DIR, F_DIR, A_SHARP_DIR, D_SHARP_DIR, G_SHARP_DIR, C_SHARP_DIR, F_SHARP_DIR, B_DIR, E_DIR = \
+        range(0, 360, 30)
+
+
+'''
+for data in NoteVectorDirection:
+    print('{:15} = {}'.format(data.name, data.value))
+'''
+
+
+@dataclass
+class NoteVector:
+    length: float
+    direction: float
+
+
+@dataclass
+class SignatureOfFifths:
+    def __init__(self):
+        self.signature = {Note.A: NoteVector(0, NoteVectorDirection.A_DIR.value),
+                          Note.D: NoteVector(0, NoteVectorDirection.D_DIR.value),
+                          Note.G: NoteVector(0, NoteVectorDirection.G_DIR.value),
+                          Note.C: NoteVector(0, NoteVectorDirection.C_DIR.value),
+                          Note.F: NoteVector(0, NoteVectorDirection.F_DIR.value),
+                          Note.C_SHARP: NoteVector(0, NoteVectorDirection.C_SHARP_DIR.value),
+                          Note.D_SHARP: NoteVector(0, NoteVectorDirection.D_SHARP_DIR.value),
+                          Note.E: NoteVector(0, NoteVectorDirection.E_DIR.value),
+                          Note.F_SHARP: NoteVector(0, NoteVectorDirection.F_SHARP_DIR.value),
+                          Note.G_SHARP: NoteVector(0, NoteVectorDirection.G_SHARP_DIR.value),
+                          Note.A_SHARP: NoteVector(0, NoteVectorDirection.A_SHARP_DIR.value),
+                          Note.B: NoteVector(0, NoteVectorDirection.B_DIR.value)}
+
+
+class SignatureOfFifthsUtility:
+    """
+        The function now counts the pitches frequency on the basis of the number of track's messages that
+        have certain pitch's code - the more messages the more the note.
+    """
+
+    def count_notes_in_track(self, track) -> {}:
+        msg_types = []
+        notes = {Note.C: 0, Note.C_SHARP: 0, Note.D: 0, Note.D_SHARP: 0, Note.E: 0, Note.F: 0, Note.F_SHARP: 0,
+                 Note.G: 0, Note.G_SHARP: 0, Note.A: 0, Note.A_SHARP: 0, Note.B: 0}
+        for msg in track:
+            if msg.type == 'note_on':
+                notes[Note(msg.note % 12)] += 1
+                msg_types.append(msg.type)
+        print("MESSAGE TYPES: " + str(Counter(msg_types).keys()))
+        print("MESSAGE TYPES OCCURRENCE FREQUENCY: " + str(Counter(msg_types).values()))
+        return notes
+
+    def calculate_signature_of_fifths(self, notes) -> SignatureOfFifths:
+        signature = SignatureOfFifths()
+        for name, value in zip(notes.keys(), notes.values()):
+            signature.signature[Note(name)].length = value / sum(notes.values()) if sum(notes.values()) != 0 else 0
+        return signature
--- midi_experiments/midi_experiments.py
+++ midi_experiments/midi_experiments.py
@@ -1,6 +1,6 @@
 from mido import MidiFile
 import paths as pth
-from signature_of_fifths_functions import SignatureOfFifthsUtility
+from functions.signature_of_fifths_functions import SignatureOfFifthsUtility
 
 mid = MidiFile(pth.MIDI_FILES + 'VampireKillerCV1.mid', clip=True)
 print(mid)
@@ -28,7 +28,12 @@ for track in mid.tracks:
     print("\n")
     print("NOTES\n")
     notes_dict = sig_util.count_notes_in_track(track)
+    print("NOTES DICT: ")
     for x, y in notes_dict.items():
+        print(x, y)
+    signature = sig_util.calculate_signature_of_fifths(notes_dict)
+    print("SIGNATURE DICT: ")
+    for x, y in signature.signature.items():
         print(x, y)
     print("\n")
     '''
