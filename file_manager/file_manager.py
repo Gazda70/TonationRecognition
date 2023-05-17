9b757de447e98cf8a793ebf53b59510fe9407bff
Gazda70 <pjgazda@interia.pl>
2023-05-17 15:39:24 +0200

    Add tonal profiles information and decision changes summary to file output.

--- file_manager/file_manager.py
+++ file_manager/file_manager.py
@@ -38,7 +38,6 @@ class MidiReader:
         worksheet = workbook.add_worksheet()
         analysis_result_number = 1
         multiple_axes_cases_number = 1
-        previous_tonation = None
         worksheet.write('E' + str(multiple_axes_cases_number), "MOVING_WINDOW_RESULTS")
         multiple_axes_cases_number += 1
         for result in moving_window_results:
@@ -109,7 +108,26 @@ class MidiReader:
         multiple_axes_cases_number = 0
         worksheet.write('E' + str(multiple_axes_cases_number), "EXPANDING_WINDOW_RESULTS")
         multiple_axes_cases_number += 1
+        previous_tonation = None
+        previous_tonation_profile_KS = None
+        previous_tonation_profile_AS = None
+        previous_tonation_profile_T = None
         decision_change_index = 1
+        decision_change_index_profile_KS = 1
+        decision_change_index_profile_AS = 1
+        decision_change_index_profile_T = 1
+        decision_change_counter = 1
+        decision_change_counter_profile_KS = 1
+        decision_change_counter_profile_AS = 1
+        decision_change_counter_profile_T = 1
+        worksheet.write('I' + str(multiple_axes_cases_number), "MAIN ALGORITHM DECISION CHANGES")
+        decision_change_index += 1
+        worksheet.write('K' + str(decision_change_index_profile_KS), "KS PROFILE DECISION CHANGES")
+        decision_change_index_profile_KS += 1
+        worksheet.write('M' + str(decision_change_index_profile_AS), "AS PROFILE DECISION CHANGES")
+        decision_change_index_profile_AS += 1
+        worksheet.write('O' + str(decision_change_index_profile_T), "T PROFILE DECISION CHANGES")
+        decision_change_index_profile_T += 1
         for result in expanding_window_results:
             # Use the worksheet object to write
             # data via the write() method.
@@ -127,6 +145,7 @@ class MidiReader:
                 worksheet.write('H' + str(multiple_axes_cases_number), result["WINDOW_END"])
                 multiple_axes_cases_number += 4
             if previous_tonation is not None and previous_tonation != result["RESULT"]:
+                decision_change_counter += 1
                 worksheet.write('I' + str(decision_change_index), "DECISION CHANGE")
                 worksheet.write('J' + str(decision_change_index), str(previous_tonation) + "->" + result["RESULT"])
                 decision_change_index += 1
@@ -177,14 +196,62 @@ class MidiReader:
             worksheet.write('B' + str(analysis_result_number), result["KS_RESULTS"])
             analysis_result_number += 1
 
+            if previous_tonation_profile_KS is not None and previous_tonation_profile_KS != result["KS_RESULTS"]:
+                decision_change_counter_profile_KS += 1
+                worksheet.write('K' + str(decision_change_index_profile_KS), "DECISION CHANGE")
+                worksheet.write('L' + str(decision_change_index_profile_KS), str(previous_tonation_profile_KS) + "->" + result["KS_RESULTS"])
+                decision_change_index_profile_KS += 1
+                worksheet.write('K' + str(decision_change_index_profile_KS), "WINDOW_START")
+                worksheet.write('L' + str(decision_change_index_profile_KS), result["WINDOW_START"])
+                decision_change_index_profile_KS += 1
+                worksheet.write('K' + str(decision_change_index_profile_KS), "WINDOW_END")
+                worksheet.write('L' + str(decision_change_index_profile_KS), result["WINDOW_END"])
+                decision_change_index += 4
+            previous_tonation_profile_KS = result["KS_RESULTS"]
+
             worksheet.write('A' + str(analysis_result_number), "AS_RESULTS")
             worksheet.write('B' + str(analysis_result_number), result["AS_RESULTS"])
             analysis_result_number += 1
 
+            if previous_tonation_profile_AS is not None and previous_tonation_profile_AS != result["AS_RESULTS"]:
+                decision_change_counter_profile_AS += 1
+                worksheet.write('M' + str(decision_change_index_profile_AS), "DECISION CHANGE")
+                worksheet.write('N' + str(decision_change_index_profile_AS), str(previous_tonation_profile_AS) + "->" + result["AS_RESULTS"])
+                decision_change_index_profile_AS += 1
+                worksheet.write('M' + str(decision_change_index_profile_AS), "WINDOW_START")
+                worksheet.write('N' + str(decision_change_index_profile_AS), result["WINDOW_START"])
+                decision_change_index_profile_AS += 1
+                worksheet.write('M' + str(decision_change_index_profile_AS), "WINDOW_END")
+                worksheet.write('N' + str(decision_change_index_profile_AS), result["WINDOW_END"])
+                decision_change_index_profile_AS += 4
+            previous_tonation_profile_AS = result["AS_RESULTS"]
+
             worksheet.write('A' + str(analysis_result_number), "T_RESULTS")
             worksheet.write('B' + str(analysis_result_number), result["T_RESULTS"])
 
+            if previous_tonation_profile_T is not None and previous_tonation_profile_T != result["T_RESULTS"]:
+                decision_change_counter_profile_T += 1
+                worksheet.write('O' + str(decision_change_index_profile_T), "DECISION CHANGE")
+                worksheet.write('P' + str(decision_change_index_profile_T), str(previous_tonation_profile_T) + "->" + result["T_RESULTS"])
+                decision_change_index_profile_T += 1
+                worksheet.write('O' + str(decision_change_index_profile_T), "WINDOW_START")
+                worksheet.write('P' + str(decision_change_index_profile_T), result["WINDOW_START"])
+                decision_change_index_profile_T += 1
+                worksheet.write('O' + str(decision_change_index_profile_T), "WINDOW_END")
+                worksheet.write('P' + str(decision_change_index_profile_T), result["WINDOW_END"])
+                decision_change_index_profile_T += 4
+            previous_tonation_profile_T = result["T_RESULTS"]
+
             analysis_result_number += 2
+
+        worksheet.write('I' + str(decision_change_index), "MAIN ALGORITHM DECISION CHANGE COUNT")
+        worksheet.write('J' + str(decision_change_index), decision_change_counter)
+        worksheet.write('K' + str(decision_change_index_profile_KS), "KS DECISION CHANGE COUNT")
+        worksheet.write('L' + str(decision_change_index_profile_KS), decision_change_counter_profile_KS)
+        worksheet.write('M' + str(decision_change_index_profile_AS), "AS DECISION CHANGE COUNT")
+        worksheet.write('N' + str(decision_change_index_profile_AS), decision_change_counter_profile_AS)
+        worksheet.write('O' + str(decision_change_index_profile_T), "T DECISION CHANGE COUNT")
+        worksheet.write('P' + str(decision_change_index_profile_T), decision_change_counter_profile_T)
 
 
         # Finally, close the Excel file
